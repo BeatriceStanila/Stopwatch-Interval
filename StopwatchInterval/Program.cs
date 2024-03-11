e45de4cb23bfe83a7de233d7313bb50f251fb7c8
btxoana <beatrice.stanila@gmail.com>
2024-03-11 15:12:39 +0000

    created the stopwatch functionality

--- StopwatchInterval/Program.cs
+++ StopwatchInterval/Program.cs
@@ -10,6 +10,27 @@
             //initialize  a new instance for StopWatch class
             var stopwatch = new StopWatch();
 
+            //create a for loop to start/stop the watch 2 times
+            for ( int i = 0; i < 2; i++)
+            {
+                stopwatch.StartTime();
+
+                //add a delay of 1000ms
+                Thread.Sleep(1000);
+
+
+                stopwatch.StopTime();
+
+                //console log the interval
+                Console.WriteLine("Duration: " + stopwatch.GetInterval());
+
+                Console.WriteLine("Press enter so start the watch one more time");
+
+                Console.ReadLine();
+            }
+
+
+
         }
     }
 }
